47c51a9cd85558bd1b0432368b77550da64cff00
Said-Work <al_sooti933@hotmail.com>
2022-01-05 12:26:58 +0400

    f

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,14 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "vue/no-parsing-error": [
+      "error", 
+      {
+          "invalid-first-character-of-tag-name": false,
+      }
+    ],
+    "no-unused-vars": 'warn',
+    'vue/no-unused-components': 'warn'
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<n-space vertical size="large" :style="{padding: 24 + 'px'}">
+  <hello-world></hello-world>
+  <d-table></d-table>
+</n-space>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import { NSpace } from 'naive-ui'
+import DTable from './components/DTable.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    NSpace,
+    DTable
+    // NLayout
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
